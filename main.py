b2044d7e00599d711e74abe0a15202a30d195a53
gbaratie <guillaume.baratier@live.fr>
2025-03-03 23:31:29 +0100

    Update main.py
    Render configurations

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import os
 from multiprocessing import Process
 
 def run_uvicorn():
-    uvicorn.run("rest.api:rest_api", host="127.0.0.1", port=8000)
+PORT = int(os.getenv("PORT", 8000))  # Render définit un PORT dynamique
+uvicorn.run("rest.api:rest_api", host="0.0.0.0", port=PORT)
 
 def run_streamlit():
     # Construct the absolute path to Home.py
-    home_py_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'web_app/Home.py'))
-    subprocess.run(["streamlit", "run", home_py_path])
+    PORT = int(os.getenv("PORT", 8501))  # Streamlit utilise 8501 par défaut
+    subprocess.run(["streamlit", "run", home_py_path, "--server.address", "0.0.0.0", "--server.port", str(PORT)])
 
 if __name__ == "__main__":
     p1 = Process(target=run_uvicorn)
