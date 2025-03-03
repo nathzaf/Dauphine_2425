195f51a1013a93b83f569868a887bd601df663b7
gbaratie <guillaume.baratier@live.fr>
2025-03-03 23:35:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import subprocess
 import uvicorn
 import os
-
 from multiprocessing import Process
 
 def run_uvicorn():
-PORT = int(os.getenv("PORT", 8000))  # Render définit un PORT dynamique
-uvicorn.run("rest.api:rest_api", host="0.0.0.0", port=PORT)
+    PORT = int(os.getenv("PORT", 8000))  # Render définit un PORT dynamique
+    uvicorn.run("rest.api:rest_api", host="0.0.0.0", port=PORT)
 
 def run_streamlit():
-    # Construct the absolute path to Home.py
+    home_py_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'web_app/Home.py'))
     PORT = int(os.getenv("PORT", 8501))  # Streamlit utilise 8501 par défaut
     subprocess.run(["streamlit", "run", home_py_path, "--server.address", "0.0.0.0", "--server.port", str(PORT)])
 
 if __name__ == "__main__":
     p1 = Process(target=run_uvicorn)
     p2 = Process(target=run_streamlit)
-    
+
     p1.start()
     p2.start()
-    
+
     p1.join()
     p2.join()
