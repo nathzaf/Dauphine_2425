5c178aca078fd2225ae7beab6b4ea30f345e0bab
Qinming JIANG <qinming.jiang@datanumia.com>
2025-03-04 16:55:14 +0100

    Add members page with project team details

---
+++ web_app/pages/3_Members.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Page Title
+st.title("Project members")
+
+# Introduction
+st.markdown("""
+This project is developed by a group of students from Dauphine University. The group members are:
+- **Student 1**: Qinming JIANG, Developer JAVA
+- **Student 2**: Nathan ZAFIZARA-BENETRIX, Developer JAVA
+- **Student 3**: Wenxin CHEN, Technical Support
+- **Student 4**: Wenbo ZHAO, Data Analyst
+- **Student 5**: Julien ZHAO, Data Engineer
+""")
