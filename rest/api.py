89f09c21f5115dbac8fde0b785ac8fe27d84937d
fookinhell <foukin-dev+github@slmail.me>
2025-03-09 17:04:55 +0100

    dependencies -> setup

--- rest/api.py
+++ rest/api.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 
-from rest.dependencies import generator_controller_adapter as controller
-from rest.endpoint.generator_rest_adapter import GeneratorRestAdapter
+from rest.setup import create_generator_rest_adapter
 from rest.endpoint.root import router as root_router
 
 rest_api = FastAPI()
 
 rest_api.include_router(root_router)
 
-generator_rest_adapter = GeneratorRestAdapter(controller)
+generator_rest_adapter = create_generator_rest_adapter()
 rest_api.include_router(generator_rest_adapter.get_router())
