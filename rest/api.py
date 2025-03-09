8f71e27a1b8d0df7ce60ea64b9f37acc9955a31b
fookinhell <foukin-dev+github@slmail.me>
2025-03-09 16:58:44 +0100

    clean rest adapter

--- rest/api.py
+++ rest/api.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
-from rest.endpoint.chat import router as chat_router
+from rest.dependencies import generator_controller_adapter as controller
+from rest.endpoint.generator_rest_adapter import GeneratorRestAdapter
 from rest.endpoint.root import router as root_router
 
 rest_api = FastAPI()
 
 rest_api.include_router(root_router)
-rest_api.include_router(chat_router)
+
+generator_rest_adapter = GeneratorRestAdapter(controller)
+rest_api.include_router(generator_rest_adapter.get_router())
