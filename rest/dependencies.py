2eb601400addf34afe4646aa7aafcae2e12fcd91
GUILLAUME BARATIER <guillaume.baratier@live.fr>
2025-03-12 16:52:46 +0100

    update

--- rest/dependencies.py
+++ rest/dependencies.py
@@ -2,10 +2,10 @@ from domain.adapter.generator_controller_adapter import GeneratorControllerAdapt
 from domain.service.text_generation_service import TextGenerationService
 from domain.service.history_service import HistoryService
 from infrastructure.adapter.text_generator_adapter import TextGeneratorAdapter
-from infrastructure.adapter.history_adapter import HistoryAdapter
+from infrastructure.adapter.json_history_adapter import JSONHistoryAdapter
 
 text_generator_adapter = TextGeneratorAdapter()
 text_generation_service = TextGenerationService(text_generator_adapter)
-history_adapter = HistoryAdapter()
+history_adapter = JSONHistoryAdapter(file_path="historic.json")
 history_service = HistoryService(history_adapter)
 generator_controller_adapter = GeneratorControllerAdapter(text_generation_service, history_service)
