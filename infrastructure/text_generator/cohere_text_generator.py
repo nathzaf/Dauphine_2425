42869b5c71f46c10d7027743eea92120c2d707ce
GUILLAUME BARATIER <guillaume.baratier@live.fr>
2025-03-12 18:05:15 +0100

    new

--- infrastructure/text_generator/cohere_text_generator.py
+++ infrastructure/text_generator/cohere_text_generator.py
@@ -1,7 +1,9 @@
-
+import sys
 import os
 from dotenv import load_dotenv
 import cohere
+
+sys.stdin = open(os.devnull)
 
 load_dotenv()
 COHERE_API_KEY = os.environ.get('COHERE_API_KEY')
@@ -10,7 +12,9 @@ class CohereTextGenerator():
     def __init__(self):
         self.client = cohere.ClientV2(COHERE_API_KEY)
 
-    def generate_text(self, prompt: str) -> str:
+    def generate_text(self, prompt: str, chat_history=None) -> str:
+        if chat_history is None:
+            chat_history = []
         response = self.client.chat(
             model="command-r-plus-08-2024",
             chat_history=chat_history,
