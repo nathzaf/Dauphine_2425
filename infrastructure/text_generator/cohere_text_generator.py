5caa2a3396f738c787398edb0b965af2c7222bd6
GUILLAUME BARATIER <guillaume.baratier@live.fr>
2025-03-13 18:39:40 +0100

    Revert "new"
    This reverts commit 42869b5c71f46c10d7027743eea92120c2d707ce.

--- infrastructure/text_generator/cohere_text_generator.py
+++ infrastructure/text_generator/cohere_text_generator.py
@@ -1,9 +1,7 @@
-import sys
+
 import os
 from dotenv import load_dotenv
 import cohere
-
-sys.stdin = open(os.devnull)
 
 load_dotenv()
 COHERE_API_KEY = os.environ.get('COHERE_API_KEY')
@@ -12,9 +10,7 @@ class CohereTextGenerator():
     def __init__(self):
         self.client = cohere.ClientV2(COHERE_API_KEY)
 
-    def generate_text(self, prompt: str, chat_history=None) -> str:
-        if chat_history is None:
-            chat_history = []
+    def generate_text(self, prompt: str) -> str:
         response = self.client.chat(
             model="command-r-plus-08-2024",
             chat_history=chat_history,
